4ba621d1ea9b08e1ea61968214cc03b1fcc69ee7
shardge <shardge@126.com>
2015-11-08 15:25:27 +0800

    default url

--- core/components/login/main.js
+++ core/components/login/main.js
@@ -52,6 +52,9 @@ angular.module('mm.core.login', [])
         url: '/site',
         templateUrl: 'core/components/login/templates/site.html',
         controller: 'mmLoginSiteCtrl',
+		onEnter: function($state){
+		$state.go('mm_login.credentials',{siteurl:'http://glearning.tju.edu.cn'});
+		}
 		
     })
 
