cdb814150bb598394896ecfd66516f7b59267146
shardge <shardge@126.com>
2015-12-03 00:17:15 +0800

    tvet

--- core/components/login/main.js
+++ core/components/login/main.js
@@ -53,7 +53,7 @@ angular.module('mm.core.login', [])
         templateUrl: 'core/components/login/templates/site.html',
         controller: 'mmLoginSiteCtrl',
 		onEnter: function($state){
-		$state.go('mm_login.credentials',{siteurl:'http://glearning.tju.edu.cn'});
+		$state.go('mm_login.credentials',{siteurl:'http://211.81.50.167:8080'});
 		}
 		
     })
