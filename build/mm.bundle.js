ac3cc35c9fa5c7893924c9bbe7092e19b0987e0b
Dosimbek <dosim@mail.ru>
2015-12-22 14:33:17 +0600

    bundle.js

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -5190,7 +5190,10 @@ angular.module('mm.core.login', [])
     .state('mm_login.site', {
         url: '/site',
         templateUrl: 'core/components/login/templates/site.html',
-        controller: 'mmLoginSiteCtrl'
+        controller: 'mmLoginSiteCtrl',
+		onEnter: function($state) {
+        $state.go('mm_login.credentials', {siteurl: 'http://e.neu.kz'});
+    }
     })
     .state('mm_login.credentials', {
         url: '/cred',
