486257c2655276b8be64602fada137c9ba6f3fa0
mohdismailj <mohdismailj@gmail.com>
2018-03-23 09:43:27 +0530

    Add files via upload

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -10845,7 +10845,7 @@ angular.module('mm.core.login', [])
         templateUrl: 'core/components/login/templates/site.html',
         controller: 'mmLoginSiteCtrl',
         onEnter: function($state) {
-            $state.go('mm_login.credentials', { siteurl: 'http://blp.skillsbreeder.com' });
+            $state.go('mm_login.credentials', { siteurl: 'https://www.skillsbreeder.com' });
         }
     })
     .state('mm_login.credentials', {
