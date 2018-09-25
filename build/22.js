de6c7b6a6e76dcba28fb44b17ea86ac141c82a7d
Travis CI User <travis@example.org>
2018-09-25 13:48:03 +0000

    Travis build: 21

--- build/22.js
+++ build/22.js
@@ -1,6 +1,6 @@
 webpackJsonp([22],{
 
-/***/ 1823:
+/***/ 1825:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
--- build/67.js
+++ build/67.js
@@ -1,6 +1,6 @@
 webpackJsonp([67],{
 
-/***/ 1825:
+/***/ 1824:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
