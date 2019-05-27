2149104aca69a0a9680a2d64581dea45cd7b5379
Travis CI User <travis@example.org>
2019-05-27 09:29:22 +0000

    Travis build: 440

--- build/68.js
+++ build/68.js
@@ -25,7 +25,7 @@ var user = __webpack_require__(43);
 var dom = __webpack_require__(4);
 
 // EXTERNAL MODULE: ./src/core/settings/providers/helper.ts
-var helper = __webpack_require__(705);
+var helper = __webpack_require__(704);
 
 // EXTERNAL MODULE: ./src/addon/messageoutput/providers/delegate.ts
 var delegate = __webpack_require__(372);
