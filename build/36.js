8e9e7882668d84081f5ba7fafc71b94282ec00b7
Travis CI User <travis@example.org>
2019-06-05 14:57:34 +0000

    Travis build: 461

--- build/36.js
+++ build/36.js
@@ -1,6 +1,6 @@
 webpackJsonp([36],{
 
-/***/ 1980:
+/***/ 1970:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -31,7 +31,7 @@ var dom = __webpack_require__(4);
 var config = __webpack_require__(174);
 
 // EXTERNAL MODULE: ./src/core/settings/providers/helper.ts
-var helper = __webpack_require__(705);
+var helper = __webpack_require__(704);
 
 // CONCATENATED MODULE: ./src/core/settings/pages/synchronization/synchronization.ts
 // (C) Copyright 2015 Martin Dougiamas
--- build/41.js
+++ build/41.js
@@ -1,6 +1,6 @@
 webpackJsonp([41],{
 
-/***/ 1965:
+/***/ 1964:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
