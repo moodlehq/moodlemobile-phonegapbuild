d934adf1777a00a4266679aedfedcd78c0f6bc81
Travis CI User <travis@example.org>
2019-06-27 12:16:19 +0000

    Travis build: 469

--- build/36.js
+++ build/36.js
@@ -1,6 +1,6 @@
 webpackJsonp([36],{
 
-/***/ 2001:
+/***/ 1980:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
--- build/95.js
+++ build/95.js
@@ -1,6 +1,6 @@
 webpackJsonp([95],{
 
-/***/ 1990:
+/***/ 1991:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
